ae2096e04d5b8e607d1f1755985b4593b8faa101
Sebastian Wittenkamp <swittenkamp@pivotal.io>
2017-11-15 23:05:00 -0800

    frist

---
+++ src/main/java/io/pivotal/rye/RyeApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.rye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyeApplication.class, args);
+	}
+}
