6f9e41d953b9207bd06f4a5ae9c650a577c94d11
Sebastian Wittenkamp <swittenkamp@pivotal.io>
2017-11-16 17:16:48 -0800

    more changes :)

--- src/main/java/io/pivotal/rye/brewing/Barrel.java
+++ src/main/java/io/pivotal/rye/brewing/Barrel.java
@@ -1,16 +1,14 @@
 package io.pivotal.rye.brewing;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Barrel {
+
     private String inscription;
 
-    public Barrel() {
-        this("The Default");
-    }
-
-    public Barrel(String inscription) {
+    public Barrel(@Value("${barrel.inscription}") String inscription) {
         this.inscription = inscription;
     }
 
